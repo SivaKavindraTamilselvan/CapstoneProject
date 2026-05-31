50919bdf979307ed2ab83108bd9af5f9783a388c
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-01 02:07:20 +0530

    authentication controller is added

---
+++ Backend/Ecommerce.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+using Ecommerce.DTOs;
+using Ecommerce.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthentication _authentication;
+    public AuthenticationController(IAuthentication authentication)
+    {
+        _authentication = authentication;
+    }
+    [HttpPost("Register")]
+    public async Task<ActionResult<ResponseRegisterUserDTO>> RegisterUser(RequestRegisterUserDTO requestRegisterUserDTO)
+    {
+        var result = await _authentication.Register(requestRegisterUserDTO);
+        return Ok(result);
+    }
+}
