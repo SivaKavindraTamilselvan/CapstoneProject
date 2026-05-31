d9116788f964ffb5527c8084d72fa458a0b61eb1
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-31 21:02:33 +0530

    icollection added for the attribute master

--- Backend/Ecommerce.Models/Models/Products/MasterTables/Attribute.cs
+++ Backend/Ecommerce.Models/Models/Products/MasterTables/Attribute.cs
@@ -3,4 +3,5 @@ public class AttributeMaster
 {
     public int AttributeMasterId { get; set; }
     public string AttributeName { get; set; } = string.Empty;
+    public ICollection<ProductVariantAttribute> ProductVariantAttributes {get;set;} = new List<ProductVariantAttribute>();
 }
