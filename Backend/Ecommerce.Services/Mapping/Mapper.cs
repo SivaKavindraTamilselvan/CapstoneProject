bea9edffb1a5a428cdf6074ff2fbcae7df780c24
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-01 10:08:46 +0530

    mapper made to avoid string space at start and end with trim

--- Backend/Ecommerce.Services/Mapping/Mapper.cs
+++ Backend/Ecommerce.Services/Mapping/Mapper.cs
@@ -8,8 +8,14 @@ namespace Ecommerce.Mappers
     {
         public MappingProfile()
         {
-            CreateMap<RequestRegisterUserDTO, User>().ForMember(dest => dest.Password, opt => opt.Ignore()).ForMember(dest => dest.HashedKey, opt => opt.Ignore());
-            CreateMap<User,ResponseRegisterUserDTO>();
+            CreateMap<RequestRegisterUserDTO, User>()
+            .ForMember(dest => dest.Password, opt => opt.Ignore())
+            .ForMember(dest => dest.HashedKey, opt => opt.Ignore())
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Trim().ToLower()))
+            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName.Trim()))
+            .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName.Trim()));
+
+            CreateMap<User, ResponseRegisterUserDTO>();
 
             CreateMap<RequestLoginUserDTO, User>();
         }
