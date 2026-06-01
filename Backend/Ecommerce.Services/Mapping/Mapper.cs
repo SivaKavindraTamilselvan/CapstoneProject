5ee70e63108bfbefdb4e05e080d600bdd70e2141
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-01 12:41:01 +0530

    mapper for the regisrtaion added

--- Backend/Ecommerce.Services/Mapping/Mapper.cs
+++ Backend/Ecommerce.Services/Mapping/Mapper.cs
@@ -11,6 +11,7 @@ namespace Ecommerce.Mappers
             CreateMap<RequestRegisterUserDTO, User>()
             .ForMember(dest => dest.Password, opt => opt.Ignore())
             .ForMember(dest => dest.HashedKey, opt => opt.Ignore())
+            .ForMember(dest => dest.RoleId, opt => opt.Ignore())
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Trim().ToLower()))
             .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName.Trim()))
             .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName.Trim()));
@@ -23,6 +24,17 @@ namespace Ecommerce.Mappers
             CreateMap<User, TokenRequest>()
             .ForMember(dest => dest.AdminRoleId, opt => opt.MapFrom(src => src.AdminUsers != null ? src.AdminUsers.AdminRoleId : (int?)null))
             .ForMember(dest => dest.VendorRoleId, opt => opt.MapFrom(src => src.VendorUser != null ? src.VendorUser.VendorRoleId : (int?)null));
+
+            CreateMap<RequestRegisterAdminDTO, RequestRegisterUserDTO>()
+            .ForMember(dest => dest.Password, opt => opt.Ignore())
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Trim().ToLower()))
+            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName.Trim()))
+            .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName.Trim()));
+
+            CreateMap<RequestRegisterAdminDTO,AdminUser>();
+
+            CreateMap<RequestRegisterVendorDTO,Vendor>();
+            CreateMap<Vendor,ResponseRegisterVendorDTO>();
         }
     }
 }
