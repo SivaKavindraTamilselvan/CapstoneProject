6acdcc8ef7fd5f7c9347d4105ce7ef8b5a6e95c6
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-01 10:43:13 +0530

    removed comment line for the login

--- Backend/Ecommerce.Services/Interfaces/IAuthentication.cs
+++ Backend/Ecommerce.Services/Interfaces/IAuthentication.cs
@@ -5,7 +5,7 @@ namespace Ecommerce.Services.Interfaces;
 public interface IAuthentication
 {
     public Task<ResponseRegisterUserDTO> Register(RequestRegisterUserDTO requestRegisterUserDTO);
-    //public Task<ResponseLoginUserDTO> Login(RequestLoginUserDTO requestLoginUserDTO);
+    public Task<ResponseLoginUserDTO> Login(RequestLoginUserDTO requestLoginUserDTO);
     //public Task<ResponseRegisterAdminDTO> RegisterAdmin(RequestRegisterAdminDTO requestRegisterAdminDTO);
     //public Task<ResponseRegisterVendorDTO> RegisterVendor(RequestRegisterVendorDTO requestRegisterVendorDTO);
 }
