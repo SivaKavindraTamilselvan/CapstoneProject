088fe3c46a2314b72f6904e3277b6d765a67f0e9
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-02 22:53:22 +0530

    removed unwanted params

--- Backend/Ecommerce.Services/Services/Admin/AdminProductService/MainProductService.cs
+++ Backend/Ecommerce.Services/Services/Admin/AdminProductService/MainProductService.cs
@@ -1,26 +1,19 @@
 using AutoMapper;
-using Ecommerce.DTOs;
 using Ecommerce.Repositories.Interfaces;
 using Ecommerce.Services.Interfaces;
 
 public partial class AdminProductService : IAdminProductService
 {
-    private readonly IAuthentication _authentication;
-    private readonly IVendorRepsository _vendorRepsository;
     private readonly IAdminUserRepsository _adminUserRepsository;
     private readonly IProductRepsository _productRepsository;
-    private readonly IVendorUserRepsository _vendorUserRepsository;
     private readonly IProductCategoryRepsository _productCategoryRepsository;
     private readonly IProductSubCategoryRepsository _productSubCategoryRepsository;
     private readonly IProductSubCategoryAttributeRepsository _productSubCategoryAttributeRepsository;
     private readonly IAttributeRepsository _attributeRepsository;
     private readonly IMapper _mapper;
-    public AdminProductService(IAuthentication authentication,IMapper mapper,IVendorRepsository vendorRepsository,IAdminUserRepsository adminUserRepsository,IProductRepsository productRepsository,IVendorUserRepsository vendorUserRepsository,IProductCategoryRepsository productCategoryRepsository,IProductSubCategoryRepsository productSubCategoryRepsository,IAttributeRepsository attributeRepsository,IProductSubCategoryAttributeRepsository productSubCategoryAttributeRepsository)
+    public AdminProductService(IMapper mapper,IAdminUserRepsository adminUserRepsository,IProductRepsository productRepsository,IProductCategoryRepsository productCategoryRepsository,IProductSubCategoryRepsository productSubCategoryRepsository,IAttributeRepsository attributeRepsository,IProductSubCategoryAttributeRepsository productSubCategoryAttributeRepsository)
     {
-        _authentication = authentication;
-        _vendorRepsository = vendorRepsository;
         _adminUserRepsository = adminUserRepsository;
-        _vendorUserRepsository = vendorUserRepsository;
         _productRepsository = productRepsository;
         _productCategoryRepsository = productCategoryRepsository;
         _productSubCategoryRepsository = productSubCategoryRepsository;
