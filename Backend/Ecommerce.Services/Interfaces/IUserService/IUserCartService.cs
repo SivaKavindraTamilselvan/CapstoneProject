88585e37d9d126006cbc971b6b5cbf7ee7e473f2
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-03 00:25:41 +0530

    functions of cart are added in the interface

--- Backend/Ecommerce.Services/Interfaces/IUserService/IUserCartService.cs
+++ Backend/Ecommerce.Services/Interfaces/IUserService/IUserCartService.cs
@@ -1,5 +1,10 @@
 namespace Ecommerce.Services.Interfaces;
+
 public interface IUserCartService
 {
     public Task<ResponseCartItemsDTO> AddCart(RequestAddCartItemsDTO requestAddCartItemsDTO, int userId);
+    public Task<List<ResponseCartItemsDTO>> DeleteAllCart();
+    public Task<ResponseCartItemsDTO> DeleteCart(RequestDeleteCartItemsDTO requestDeleteCartItemsDTO);
+    public Task<ResponseCartItemsDTO> UpdateCart(RequestUpdateCartItemsDTO requestUpdateCartItemsDTO);
+
 }
