ef31547cb2a3f8e4f4c95d44af125e15d3bcf2be
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-02 19:52:50 +0530

    product variant and product image is added

--- Backend/Ecommerce.Services/Interfaces/IProductService.cs
+++ Backend/Ecommerce.Services/Interfaces/IProductService.cs
@@ -5,4 +5,6 @@ namespace Ecommerce.Services.Interfaces;
 public interface IProductService
 {
     public Task<ResponseAddProduct> AddProduct(RequestAddProduct requestAddProduct,int vendorUserId);
+    public Task<ResponseAddProductVariantDTO> AddProductVariant(RequestAddProductVariantDTO requestAddProductVariantDTO);
+    public Task<ResponseAddProductImage> AddProductImage(RequestAddProductImage requestAddProductImage);
 }
