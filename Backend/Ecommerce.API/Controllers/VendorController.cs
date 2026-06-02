c41dc44421add72db8572da7ac1f86f347fc6e24
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-02 12:26:25 +0530

    vendor controller for register added

---
+++ Backend/Ecommerce.API/Controllers/VendorController.cs
@@ -0,0 +1,28 @@
+using System.Security.Claims;
+using Ecommerce.DTOs;
+using Ecommerce.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VendorController : ControllerBase
+{
+    private readonly IVendorService _vendorService;
+    public VendorController(IVendorService vendorService)
+    {
+        _vendorService = vendorService;
+    }
+
+    [Authorize(Policy = "VendorAdminOnly")]
+    [HttpPost("RegisterVendorUser")]
+    public async Task<ActionResult<ResponseRegisterVendorUserDTO>> RegisterVendorUser(RequestRegisterVendorUserDTO requestRegisterVendorUserDTO)
+    {
+        int vendorUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+        var result = await _vendorService.RegisterVendorUser(requestRegisterVendorUserDTO,vendorUserId);
+        return Ok(result);
+    }
+    
+}
