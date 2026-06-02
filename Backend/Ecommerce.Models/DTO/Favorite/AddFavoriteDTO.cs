4f23788e94d48506413e4494660baef07b972987
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-03 00:34:48 +0530

    dto for the favorites and cart are added and file renamed

---
+++ Backend/Ecommerce.Models/DTO/Favorite/AddFavoriteDTO.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+public class RequestAddFavoriteItemsDTO
+{
+    [Required(ErrorMessage = "Product Variant Id Needed")]
+    [Range(1, int.MaxValue, ErrorMessage = "Product Variant Id Should Be Greater Than 0")]
+    public int ProductVariantId {get;set;}
+}
+public class ResponseFavoriteItemsDTO
+{
+    public int FavoritesItemsId {get;set;}
+    public int FavoritesId {get;set;}
+    public int ProductVariantId {get;set;}
+}
+
--- Backend/Ecommerce.Models/DTO/Favorite/DeleteFavoriteDTO.cs
+++ Backend/Ecommerce.Models/DTO/Favorite/DeleteFavoriteDTO.cs
@@ -1,15 +1,10 @@
 using System.ComponentModel.DataAnnotations;
 
-public class RequestAddFavoriteItemsDTO
+public class RequestDeleteFavoriteItemsDTO
 {
     [Required(ErrorMessage = "Product Variant Id Needed")]
     [Range(1, int.MaxValue, ErrorMessage = "Product Variant Id Should Be Greater Than 0")]
-    public int ProductVariantId {get;set;}
-}
-public class ResponseAddFavoriteItemsDTO
-{
     public int FavoritesItemsId {get;set;}
-    public int FavoritesId {get;set;}
-    public int ProductVariantId {get;set;}
 }
+
 
