b8db1dca841906b4389e077a6594e9164c9dec21
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-02 15:23:21 +0530

    added the dependency that are needed

--- Backend/Ecommerce.Services/Admin/MainAdminService.cs
+++ Backend/Ecommerce.Services/Admin/MainAdminService.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Ecommerce.DTOs;
 using Ecommerce.Repositories.Interfaces;
 using Ecommerce.Services.Interfaces;
@@ -7,11 +8,17 @@ public partial class AdminService : IAdminService
     private readonly IAuthentication _authentication;
     private readonly IVendorRepsository _vendorRepsository;
     private readonly IAdminUserRepsository _adminUserRepsository;
-    public AdminService(IAuthentication authentication,IVendorRepsository vendorRepsository,IAdminUserRepsository adminUserRepsository)
+    private readonly IProductRepsository _productRepsository;
+    private readonly IVendorUserRepsository _vendorUserRepsository;
+    private readonly IMapper _mapper;
+    public AdminService(IAuthentication authentication,IMapper mapper,IVendorRepsository vendorRepsository,IAdminUserRepsository adminUserRepsository,IProductRepsository productRepsository,IVendorUserRepsository vendorUserRepsository)
     {
         _authentication = authentication;
         _vendorRepsository = vendorRepsository;
         _adminUserRepsository = adminUserRepsository;
+        _vendorUserRepsository = vendorUserRepsository;
+        _productRepsository = productRepsository;
+        _mapper = mapper;
     }
     public async Task<ResponseRegisterAdminDTO> RegisterAdmin(RequestRegisterAdminDTO requestRegisterAdminDTO,int adminUserId)
     {
