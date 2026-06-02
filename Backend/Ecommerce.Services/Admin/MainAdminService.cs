47e75947a1672f364e90510823fd2ea6e5d4f57f
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-02 16:39:28 +0530

    admin service are added with the product services

--- Backend/Ecommerce.Services/Admin/MainAdminService.cs
+++ Backend/Ecommerce.Services/Admin/MainAdminService.cs
@@ -10,14 +10,22 @@ public partial class AdminService : IAdminService
     private readonly IAdminUserRepsository _adminUserRepsository;
     private readonly IProductRepsository _productRepsository;
     private readonly IVendorUserRepsository _vendorUserRepsository;
+    private readonly IProductCategoryRepsository _productCategoryRepsository;
+    private readonly IProductSubCategoryRepsository _productSubCategoryRepsository;
+    private readonly IProductSubCategoryAttributeRepsository _productSubCategoryAttributeRepsository;
+    private readonly IAttributeRepsository _attributeRepsository;
     private readonly IMapper _mapper;
-    public AdminService(IAuthentication authentication,IMapper mapper,IVendorRepsository vendorRepsository,IAdminUserRepsository adminUserRepsository,IProductRepsository productRepsository,IVendorUserRepsository vendorUserRepsository)
+    public AdminService(IAuthentication authentication,IMapper mapper,IVendorRepsository vendorRepsository,IAdminUserRepsository adminUserRepsository,IProductRepsository productRepsository,IVendorUserRepsository vendorUserRepsository,IProductCategoryRepsository productCategoryRepsository,IProductSubCategoryRepsository productSubCategoryRepsository,IAttributeRepsository attributeRepsository,IProductSubCategoryAttributeRepsository productSubCategoryAttributeRepsository)
     {
         _authentication = authentication;
         _vendorRepsository = vendorRepsository;
         _adminUserRepsository = adminUserRepsository;
         _vendorUserRepsository = vendorUserRepsository;
         _productRepsository = productRepsository;
+        _productCategoryRepsository = productCategoryRepsository;
+        _productSubCategoryRepsository = productSubCategoryRepsository;
+        _productSubCategoryAttributeRepsository = productSubCategoryAttributeRepsository;
+        _attributeRepsository = attributeRepsository;
         _mapper = mapper;
     }
     public async Task<ResponseRegisterAdminDTO> RegisterAdmin(RequestRegisterAdminDTO requestRegisterAdminDTO,int adminUserId)
--- Backend/Ecommerce.Services/Interfaces/IAdminService.cs
+++ Backend/Ecommerce.Services/Interfaces/IAdminService.cs
@@ -7,4 +7,9 @@ public interface IAdminService
     public Task<ResponseReviewOfVendorDTO> ReviewVendor(RequestReviewOfVendorDTO requestReviewOfVendorDTO, int userId);
     public Task<ResponseReviewOfProductDTO> ReviewProduct(RequestReviewOfProductDTO requestReviewOfProductDTO, int adminUserId);
     public Task<ResponseRegisterAdminDTO> RegisterAdmin(RequestRegisterAdminDTO requestRegisterAdminDTO, int adminUserId);
+    public Task<ResponseAddProductCategoryDTO> AddProductCategory(RequestAddProductCategoryDTO requestAddProductCategoryDTO);
+    public Task<ResponseAddProductSubCategoryDTO> AddProductSubCategory(RequestAddProductSubCategoryDTO requestAddProductSubCategoryDTO);
+    public Task<ResponseAddAttributeDTO> AddAttribute(RequestAddAttributeDTO requestAddAttributeDTO);
+    public Task<ResponseAddProductSubCategoryAttributeDTO> AddProductSubCategoryAttribute(RequestAddProductSubCategoryAttributeDTO requestAddProductSubCategoryAttributeDTO);
+
 }
