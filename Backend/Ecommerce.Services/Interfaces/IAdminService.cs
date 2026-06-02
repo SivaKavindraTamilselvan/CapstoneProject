94c73e21189c69c5798aa7a20efa04b65f50da92
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-02 11:47:57 +0530

    comment line removed for checking and imlementing the vendor review service

--- Backend/Ecommerce.Services/Interfaces/IAdminService.cs
+++ Backend/Ecommerce.Services/Interfaces/IAdminService.cs
@@ -4,6 +4,6 @@ namespace Ecommerce.Services.Interfaces;
 
 public interface IAdminService
 {
-    //public Task<ResponseReviewOfVendorDTO> ReviewVendor(RequestReviewOfVendorDTO requestReviewOfVendorDTO,int userId);
+    public Task<ResponseReviewOfVendorDTO> ReviewVendor(RequestReviewOfVendorDTO requestReviewOfVendorDTO,int userId);
     public Task<ResponseRegisterAdminDTO> RegisterAdmin(RequestRegisterAdminDTO requestRegisterAdminDTO,int adminUserId);
 }
