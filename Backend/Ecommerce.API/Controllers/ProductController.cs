71fceb15cf2aa4055f1a6c81a9cee659bc5a7444
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-02 19:53:48 +0530

    product controller updated with variant and image

--- Backend/Ecommerce.API/Controllers/ProductController.cs
+++ Backend/Ecommerce.API/Controllers/ProductController.cs
@@ -13,7 +13,7 @@ public class ProductController : ControllerBase
     private readonly IProductService _productService;
     public ProductController(IProductService productService)
     {
-       _productService = productService;
+        _productService = productService;
     }
 
     [Authorize(Policy = "VendorOnwerAndProductVendorOnly")]
@@ -21,7 +21,21 @@ public class ProductController : ControllerBase
     public async Task<ActionResult<ResponseAddProduct>> AddProduct(RequestAddProduct requestAddProduct)
     {
         int vendorUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
-        var result = await _productService.AddProduct(requestAddProduct,vendorUserId);
+        var result = await _productService.AddProduct(requestAddProduct, vendorUserId);
+        return Ok(result);
+    }
+    [Authorize(Policy = "VendorOnwerAndProductVendorOnly")]
+    [HttpPost("AddProductImage")]
+    public async Task<ActionResult<ResponseAddProductImage>> AddProductImage(RequestAddProductImage requestAddProductImage)
+    {
+        var result = await _productService.AddProductImage(requestAddProductImage); 
+        return Ok(result);
+    }
+    [Authorize(Policy = "VendorOnwerAndProductVendorOnly")]
+    [HttpPost("AddProductVariant")]
+    public async Task<ActionResult<ResponseAddProductVariantDTO>> AddProductImage(RequestAddProductVariantDTO requestAddProductVariantDTO)
+    {
+        var result = await _productService.AddProductVariant(requestAddProductVariantDTO); 
         return Ok(result);
     }
 }
