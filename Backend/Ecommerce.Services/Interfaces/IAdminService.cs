bc82d304bb360258a82c01b435c096ed122f8bba
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-02 15:23:08 +0530

    added the review of product functionality

--- Backend/Ecommerce.Services/Interfaces/IAdminService.cs
+++ Backend/Ecommerce.Services/Interfaces/IAdminService.cs
@@ -4,6 +4,7 @@ namespace Ecommerce.Services.Interfaces;
 
 public interface IAdminService
 {
-    public Task<ResponseReviewOfVendorDTO> ReviewVendor(RequestReviewOfVendorDTO requestReviewOfVendorDTO,int userId);
-    public Task<ResponseRegisterAdminDTO> RegisterAdmin(RequestRegisterAdminDTO requestRegisterAdminDTO,int adminUserId);
+    public Task<ResponseReviewOfVendorDTO> ReviewVendor(RequestReviewOfVendorDTO requestReviewOfVendorDTO, int userId);
+    public Task<ResponseReviewOfProductDTO> ReviewProduct(RequestReviewOfProductDTO requestReviewOfProductDTO, int adminUserId);
+    public Task<ResponseRegisterAdminDTO> RegisterAdmin(RequestRegisterAdminDTO requestRegisterAdminDTO, int adminUserId);
 }
