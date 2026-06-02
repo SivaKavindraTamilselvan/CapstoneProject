38fd0a55c5c9db6640f4784d8a618b0d729bc3d9
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-02 12:25:22 +0530

    made private to public

--- Backend/Ecommerce.Services/Interfaces/IAuthentication.cs
+++ Backend/Ecommerce.Services/Interfaces/IAuthentication.cs
@@ -4,6 +4,7 @@ namespace Ecommerce.Services.Interfaces;
 
 public interface IAuthentication
 {
+    public Task<ResponseRegisterUserDTO> RegisterUser(RequestRegisterUserDTO requestRegisterUserDTO, int RoleId);
     public Task<ResponseRegisterUserDTO> Register(RequestRegisterUserDTO requestRegisterUserDTO);
     public Task<ResponseLoginUserDTO> Login(RequestLoginUserDTO requestLoginUserDTO);
     public Task<ResponseRegisterAdminDTO> RegisterAdmin(RequestRegisterAdminDTO requestRegisterAdminDTO,int adminUserId);
