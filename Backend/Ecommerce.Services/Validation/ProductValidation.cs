8d023082515632361418e272340d07f09f4b0344
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-03 20:55:39 +0530

    seperate validation is added to avoid repeated code

---
+++ Backend/Ecommerce.Services/Validation/ProductValidation.cs
@@ -0,0 +1,53 @@
+using Ecommerce.Models;
+using Ecommerce.Models.Exceptions;
+using Ecommerce.Repositories.Interfaces;
+using Ecommerce.Services.Interfaces;
+
+public class ProductValidation : IProductValidation
+{
+    private readonly IProductRepsository _productRepsository;
+    private readonly IProductVariantRepsository _productVariantRepsository;
+    private readonly IProductSubCategoryRepsository _productSubCategoryRepsository;
+    public ProductValidation(IProductSubCategoryRepsository productSubCategoryRepsository, IProductRepsository productRepsository, IProductVariantRepsository productVariantRepsository)
+    {
+        _productSubCategoryRepsository = productSubCategoryRepsository;
+        _productRepsository = productRepsository;
+        _productVariantRepsository = productVariantRepsository;
+    }
+    public async Task<ProductSubCategory> ValidateSubCategory(int subCategoryId)
+    {
+        var subCategory = await _productSubCategoryRepsository.Get(subCategoryId);
+        if (subCategory == null)
+        {
+            throw new DataNotFoundException("Product Sub Category Is Not Found");
+        }
+        return subCategory;
+    }
+    public async Task<Product> ValidateProduct(int productId)
+    {
+        var product = await _productRepsository.Get(productId);
+        if (product == null)
+        {
+            throw new DataNotFoundException("Product Not Found");
+        }
+        return product;
+    }
+    public async Task<Product> ValidateProductIfApproved(int productId)
+    {
+        var product = await ValidateProduct(productId);
+        if (product.ProductApprovalStatusId != 4)
+        {
+            throw new DataApprovalStatusException("Product Is Not Yet Approved");
+        }
+        return product;
+    }
+    public async Task<ProductVariant> ValidateProductVariant(int productVariantId)
+    {
+        var productVariant = await _productVariantRepsository.Get(productVariantId);
+        if (productVariant == null)
+        {
+            throw new DataNotFoundException("Product Variant Not Found");
+        }
+        return productVariant;
+    }
+}
---
+++ Backend/Ecommerce.Services/Validation/VendorValidation.cs
@@ -0,0 +1,44 @@
+using Ecommerce.Models;
+using Ecommerce.Models.Exceptions;
+using Ecommerce.Repositories.Interfaces;
+using Ecommerce.Services.Interfaces;
+
+public class VendorValidation :IVendorValidation
+{
+    private readonly IVendorUserRepsository _vendorUserRepsository;
+    private readonly IVendorRepsository _vendorRepsository;
+    public VendorValidation(IVendorUserRepsository vendorUserRepsository,IVendorRepsository vendorRepsository)
+    {
+        _vendorUserRepsository = vendorUserRepsository;
+        _vendorRepsository = vendorRepsository;
+    }
+    public async Task<VendorUser> ValidateVendorUser(int vendorUserId)
+    {
+        var vendorUser = await _vendorUserRepsository.GetVendorUserByUserId(vendorUserId);
+        if (vendorUser == null)
+        {
+            throw new DataNotFoundException("Vendor User Not Found");
+        }
+        return vendorUser;
+    }
+
+    public async Task<Vendor> ValidateVendor(int vendorId)
+    {
+        var vendor = await _vendorRepsository.Get(vendorId);
+        if (vendor == null)
+        {
+            throw new DataNotFoundException("Vendor Not Found");
+        }
+        return vendor;
+    }
+
+    public async Task<Vendor> ValidateVendorIfApproved(int vendorId)
+    {
+        var vendor = await ValidateVendor(vendorId);
+        if(vendor.ApprovalStatusId !=2)
+        {
+            throw new DataApprovalStatusException("The Vendor Is Not Approved Yet");
+        }
+        return vendor;
+    }
+}
