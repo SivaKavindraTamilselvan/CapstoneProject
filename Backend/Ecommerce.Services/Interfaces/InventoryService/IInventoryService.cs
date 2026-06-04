b9a1515482b4ac8aed90f5c290ee47479eda5425
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-04 14:14:20 +0530

    added the functions in inventory service

--- Backend/Ecommerce.Services/Interfaces/InventoryService/IInventoryService.cs
+++ Backend/Ecommerce.Services/Interfaces/InventoryService/IInventoryService.cs
@@ -4,5 +4,7 @@ namespace Ecommerce.Services.Interfaces;
 
 public interface IInventoryService
 {
-    
+    public Task<ResponseAddInventoryDTO> AddInventory(RequestAddInventoryDTO requestAddInventoryDTO);
+    public Task<ResponseUpdateInventoryDTO> UpdateInventory(RequestUpdateInventoryDTO requestUpdateInventoryDTO);
+
 }
