d13cac95a73c929ce36631d56af60b0ff8feef34
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-04 14:33:48 +0530

    added the get address by user id function

--- Backend/Ecommerce.Repositories/Interfaces/Users/IAddress.cs
+++ Backend/Ecommerce.Repositories/Interfaces/Users/IAddress.cs
@@ -2,6 +2,7 @@ using Ecommerce.Models;
 
 namespace Ecommerce.Repositories.Interfaces;
 
-public interface IAddressRepsository : IRepository<int,Address>
+public interface IAddressRepsository : IRepository<int, Address>
 {
+    public Task<List<Address>> GetAddressByUserId(int userId);
 }
