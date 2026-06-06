a6b8492f22cb1e8e7d3e18efd61e5a03607a1db1
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-06 15:13:27 +0530

    removed unwanted params

--- Backend/Ecommerce.Services/Validation/ProductCategoryValidation.cs
+++ Backend/Ecommerce.Services/Validation/ProductCategoryValidation.cs
@@ -7,14 +7,10 @@ public class ProductCategoryValidation : IProductCategoryValidation
 {
     private readonly IProductCategoryRepsository _productCategoryRepsository;
     private readonly IProductSubCategoryRepsository _productSubCategoryRepsository;
-    private readonly IProductSubCategoryAttributeRepsository _productSubCategoryAttributeRepsository;
-    private readonly IAttributeRepsository _attributeRepsository;
-    public ProductCategoryValidation(IProductCategoryRepsository productCategoryRepsository, IProductSubCategoryRepsository productSubCategoryRepsository, IProductSubCategoryAttributeRepsository productSubCategoryAttributeRepsository, IAttributeRepsository attributeRepsository)
+    public ProductCategoryValidation(IProductCategoryRepsository productCategoryRepsository, IProductSubCategoryRepsository productSubCategoryRepsository)
     {
         _productCategoryRepsository = productCategoryRepsository;
         _productSubCategoryRepsository = productSubCategoryRepsository;
-        _productSubCategoryAttributeRepsository = productSubCategoryAttributeRepsository;
-        _attributeRepsository = attributeRepsository;
     }
     public async Task<ProductSubCategory> ValidateSubCategory(int subCategoryId)
     {
@@ -24,15 +20,6 @@ public class ProductCategoryValidation : IProductCategoryValidation
             throw new DataNotFoundException("Product Sub Category Is Not Found");
         }
         return subCategory;
-    }
-    public async Task<ProductSubCategoryAttribute> ValidateProductSubCategoryAttribute(int productSubCategoryAttributeId, int productSubCategoryId)
-    {
-        var attribute = await _productSubCategoryAttributeRepsository.ValidateProductSubCategoryAttribute(productSubCategoryAttributeId, productSubCategoryId);
-        if (attribute == null)
-        {
-            throw new DataNotFoundException("Product Sub Category Attribute Not Found For This Product Sub Category");
-        }
-        return attribute;
     }
     public async Task<ProductCategory?> ValidateProductCategoryName(string ProductCategoryName)
     {
@@ -51,23 +38,5 @@ public class ProductCategoryValidation : IProductCategoryValidation
             throw new DataAlreadyRegisteredException("Already The Product Sub Category Name Is Registered");
         }
         return product;
-    }
-    public async Task<ProductSubCategoryAttribute> ValidateProductSubCategoryAttributeForAdmin(int productSubCategoryId, int AttributeId)
-    {
-        var result = await _productSubCategoryAttributeRepsository.ValidateProductSubCategoryAttribute(AttributeId, productSubCategoryId);
-        if (result == null)
-        {
-            throw new DataNotFoundException("The Sub Category is not mapped to the Attribute");
-        }
-        return result;
-    }
-    public async Task<AttributeMaster> ValidateAttributeName(string name)
-    {
-        var attribute = await _attributeRepsository.GetTheAttributeByName(name);
-        if (attribute == null)
-        {
-            throw new DataAlreadyRegisteredException("Already The Attribute Name Is Registered");
-        }
-        return attribute;
     }
 }
