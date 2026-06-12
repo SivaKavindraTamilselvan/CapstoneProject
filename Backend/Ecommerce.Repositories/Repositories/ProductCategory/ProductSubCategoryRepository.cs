cb5216f8376e1b7ae137a2708b32789cfad9ee27
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-12 14:30:29 +0530

    added flters for admin

--- Backend/Ecommerce.Repositories/Repositories/ProductCategory/ProductSubCategoryRepository.cs
+++ Backend/Ecommerce.Repositories/Repositories/ProductCategory/ProductSubCategoryRepository.cs
@@ -1,4 +1,5 @@
 using Ecommerce.Data;
+using Ecommerce.DTOs;
 using Ecommerce.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,19 +24,35 @@ public class ProductSubCategoryRepsository : AbstractRepository<int, ProductSubC
     }
 
     // get usage for admin
-    public async Task<(List<ProductSubCategory>,int totalCount)> GetAllSubProductCategoryForAdmin(bool? status,int? categoryId,int pageNumber,int pageSize)
+    public async Task<(List<ProductSubCategory>,int totalCount)> GetAllSubProductCategoryForAdmin(RequestProductSubCategoryFilter request)
     {
         var query = _ecommerceContext.ProductSubCategory.Include(a=>a.AddedByAdminUser).ThenInclude(u=>u!.User).Include(p=>p.ProductCategory).AsQueryable();
-        if(status.HasValue)
+        if(request.status.HasValue)
         {
-            query = query.Where(a=>a.IsActive == status);
+            query = query.Where(a=>a.IsActive == request.status);
         }
-        if(categoryId.HasValue)
+        if(request.ProductCategoryId.HasValue)
         {
-            query = query.Where(q=>q.ProductCategoryId == categoryId);
+            query = query.Where(q=>q.ProductCategoryId == request.ProductCategoryId);
+        }
+        if(request.ProductSubCategoryId.HasValue)
+        {
+            query = query.Where(q=>q.ProductSubCategoryId == request.ProductSubCategoryId);
+        }
+        if(request.MinimumCommissionPercentage.HasValue)
+        {
+            query = query.Where(q=>q.CommissionPercentage >= request.MinimumCommissionPercentage.Value);
+        }
+        if(request.MaximumCommissionPercentage.HasValue)
+        {
+            query = query.Where(q=>q.CommissionPercentage <= request.MaximumCommissionPercentage.Value);
+        }
+        if(request.AddedByAdminId.HasValue)
+        {
+            query = query.Where(q=>q.AddedByAdminId == request.AddedByAdminId.Value);
         }
         var totalCount = await query.CountAsync();
-        var data =  await query.OrderBy(a=>a.ProductSubCategoryName).Skip((pageNumber-1)*pageSize).Take(pageSize).ToListAsync();
+        var data =  await query.OrderBy(a=>a.ProductSubCategoryName).Skip((request.PageNumber-1)*request.PageSize).Take(request.PageSize).ToListAsync();
         return(data,totalCount);
     }
 }
