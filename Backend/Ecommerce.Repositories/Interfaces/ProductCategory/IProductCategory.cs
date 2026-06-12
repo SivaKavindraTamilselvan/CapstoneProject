5bf3107dc4d441232a8ae670413c2250ab0c65fb
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-12 14:30:47 +0530

    changed the  return type and params type

--- Backend/Ecommerce.Repositories/Interfaces/ProductCategory/IProductCategory.cs
+++ Backend/Ecommerce.Repositories/Interfaces/ProductCategory/IProductCategory.cs
@@ -1,10 +1,11 @@
+using Ecommerce.DTOs;
 using Ecommerce.Models;
 
 namespace Ecommerce.Repositories.Interfaces;
 
 public interface IProductCategoryRepsository : IRepository<int,ProductCategory>
 {
-    public Task<(List<ProductCategory>,int totalCount)> GetAllProductCategoryForAdmin(bool? status,int pageNumber,int pageSize);
+    public Task<(List<ProductCategory>,int totalCount)> GetAllProductCategoryForAdmin(RequestProductCategoryFilter request);
     public Task<List<ProductCategory>> GetAllProductCategoryForUser();
     public Task<ProductCategory?> CheckUniqueProductCategory(string productCategoryName);
 }
--- Backend/Ecommerce.Repositories/Interfaces/ProductCategory/IProductSubCategory.cs
+++ Backend/Ecommerce.Repositories/Interfaces/ProductCategory/IProductSubCategory.cs
@@ -1,10 +1,11 @@
+using Ecommerce.DTOs;
 using Ecommerce.Models;
 
 namespace Ecommerce.Repositories.Interfaces;
 
 public interface IProductSubCategoryRepsository : IRepository<int, ProductSubCategory>
 {
-    public Task<(List<ProductSubCategory>, int totalCount)> GetAllSubProductCategoryForAdmin(bool? status, int? categoryId, int pageNumber, int pageSize);
+    public Task<(List<ProductSubCategory>, int totalCount)> GetAllSubProductCategoryForAdmin(RequestProductSubCategoryFilter request);
     public Task<List<ProductSubCategory>> GetAllProductSubCategory(int productId);
     public Task<ProductSubCategory?> CheckUniqueProductSubCategory(string productSubCategoryName);
 }
