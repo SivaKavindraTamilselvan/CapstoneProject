116c78f97ad13dc1be93d4eabaacb453541633e5
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-13 02:25:20 +0530

    needed di are added for product images

--- Backend/Ecommerce.Services/Services/VendorProductImageService/MainPrductService.cs
+++ Backend/Ecommerce.Services/Services/VendorProductImageService/MainPrductService.cs
@@ -1,20 +1,27 @@
 using AutoMapper;
 using Ecommerce.Repositories.Interfaces;
 using Ecommerce.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 
 public partial class VendorProductImageService : IVendorProductImageService
 {
+    private readonly IAdminUserRepsository _adminUserRepsository;
+    private readonly INotificationService _notificationService;
+    private readonly ILogger<VendorProductImageService> _logger;
     private readonly IVendorUserValidation _vendorUserValidation;
     private readonly IProductImageRepsository _productImageRepsository;
     private readonly IProductValidation _productValidation;
     private readonly IMapper _mapper;
 
 
-    public VendorProductImageService(IVendorUserValidation vendorUserValidation,IMapper mapper,IProductImageRepsository productImageRepsository,IProductValidation productValidation)
+    public VendorProductImageService(IAdminUserRepsository adminUserRepsository,INotificationService notificationService,ILogger<VendorProductImageService> logger,IVendorUserValidation vendorUserValidation,IMapper mapper,IProductImageRepsository productImageRepsository,IProductValidation productValidation)
     {
+        _adminUserRepsository = adminUserRepsository;
+        _notificationService = notificationService;
         _vendorUserValidation = vendorUserValidation;
         _productImageRepsository = productImageRepsository;     
         _productValidation = productValidation;
         _mapper = mapper;
+        _logger = logger;
     }
 }
