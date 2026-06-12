3b9acb60382f3134c9595f65d83cae54e83b8cff
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-12 21:06:35 +0530

    the get product services are added with filter

--- Backend/Ecommerce.Services/Interfaces/IAdminService/IProductService.cs
+++ Backend/Ecommerce.Services/Interfaces/IAdminService/IProductService.cs
@@ -4,14 +4,11 @@ namespace Ecommerce.Services.Interfaces;
 
 public interface IAdminProductService
 {
-    public Task<List<ResponseGetAllProductVariant>> GetAllProductVariant(ProductVariantFilterDto filter);
+    public Task<PagedResponse<ResponseGetAllProductVariant>> GetAllProductVariant(RequestAdminProductVariantFilter filter);
     public Task<ResponseReviewOfProductDTO> DeleteProduct(RequestDeleteProductDTO requestDeleteProductDTO,int adminUserId);
     public Task<ResponseReviewOfProductVariantDTO> ReviewProductVariant(RequestReviewOfProductVariantDTO requestReviewOfProductDTO, int adminUserId);
     public Task<ResponseReviewOfProductDTO> ReviewProduct(RequestReviewOfProductDTO requestReviewOfProductDTO, int adminUserId);
-    public Task<List<ResponseAdminGetAllProductDTO>> GetAllProducts(int? approval, int? status, int? vendorId, int? subcategory, bool? hasIssues,bool? isAvailableForSale,int pageNumber,int pageSize);
-    Task<List<ResponseAdminGetStockProductDTO>> GetAllOutOfStockProducts();
-    Task<List<ResponseAdminGetStockProductDTO>> GetAllLowStockProducts(int threshold = 5);
-    Task<List<ResponseAdminGetAllProductDTO>> GetAllProductsWithPendingVariants();
+    public Task<PagedResponse<ResponseAdminGetAllProductDTO>> GetAllProductsForAdmin(RequestAdminProductFilter request);
     Task<ResponseAdminGetAllProductDTO> GetProductWithFullDetails(int productId);
 
 }
--- Backend/Ecommerce.Services/Services/AdminProductService/GetProductService.cs
+++ Backend/Ecommerce.Services/Services/AdminProductService/GetProductService.cs
@@ -2,24 +2,29 @@ using Ecommerce.DTOs;
 using Ecommerce.Models;
 using Ecommerce.Models.Exceptions;
 using Ecommerce.Services.Interfaces;
+using Org.BouncyCastle.Ocsp;
 
 public partial class AdminProductService : IAdminProductService
 {
 
-    public async Task<List<ResponseAdminGetAllProductDTO>> GetAllProducts(int? approval, int? status, int? vendorId, int? subcategory, bool? hasIssues, bool? isAvailableForSale, int pageNumber, int pageSize)
+    public async Task<PagedResponse<ResponseAdminGetAllProductDTO>> GetAllProductsForAdmin(RequestAdminProductFilter request)
     {
-        var products = await _productRepsository.GetAdminProduct(approval, status, vendorId, subcategory, pageNumber, pageSize);
+        var result = await _productRepsository.GetAdminProduct(request);
+        var products = result.items;
         var response = _mapper.Map<List<ResponseAdminGetAllProductDTO>>(products);
         for (int i = 0; i < products.Count; i++)
         {
             var validation = await _productValidation.ValidateProductChain(products[i]);
-            response[i].IsAvailableForSale = products[i].ProductApprovalStatusId == 4 && products[i].ProductStatusId == 2 && validation.IsValid && products[i]
-            .ProductVariants.Any(pv => pv.ProductApprovalStatusId == 4 && pv.ProductVariantStatusId == 2 && pv.Inventories.Any(inv => inv.AvailableQuantity > 0));
+
+            response[i].IsAvailableForSale = products[i].ProductApprovalStatusId == (int)ProductApprovalStatusEnum.Admin_Approved && products[i].ProductStatusId == (int)ProductStatusEnum.Active
+            && validation.IsValid && products[i].ProductVariants.Any(pv => pv.ProductApprovalStatusId == (int)ProductApprovalStatusEnum.Admin_Approved && pv.ProductVariantStatusId == (int)ProductStatusEnum.Active
+            && pv.Inventories.Any(inv => inv.AvailableQuantity > 0));
+
             response[i].ValidationIssues = validation.Issues;
         }
-        if (hasIssues.HasValue)
+        if (request.hasIssues.HasValue)
         {
-            if (hasIssues.Value)
+            if (request.hasIssues.Value)
             {
                 response = response.Where(p => p.ValidationIssues.Any()).ToList();
             }
@@ -28,34 +33,55 @@ public partial class AdminProductService : IAdminProductService
                 response = response.Where(p => !p.ValidationIssues.Any()).ToList();
             }
         }
-        if (isAvailableForSale.HasValue)
+        if (request.isAvailableForSale.HasValue)
         {
-            response = response.Where(p => p.IsAvailableForSale == isAvailableForSale.Value).ToList();
+            response = response.Where(p => p.IsAvailableForSale == request.isAvailableForSale.Value).ToList();
         }
-        return response;
+        return new PagedResponse<ResponseAdminGetAllProductDTO>
+        {
+            Items = response,
+            TotalCount = result.totalCount,
+            PageNumber = request.PageNumber,
+            PageSize = request.PageSize
+        };
     }
-    public async Task<List<ResponseGetAllProductVariant>> GetAllProductVariant(ProductVariantFilterDto filter)
+    public async Task<PagedResponse<ResponseGetAllProductVariant>> GetAllProductVariant(RequestAdminProductVariantFilter request)
     {
-        var product = await _productVariantRepsository.GetAllVariantsForAdmin(filter);
-        return _mapper.Map<List<ResponseGetAllProductVariant>>(product);
-    }
-    public async Task<List<ResponseAdminGetStockProductDTO>> GetAllOutOfStockProducts()
-    {
-        var products = await _productRepsository.GetAllOutOfStockProducts();
-        return _mapper.Map<List<ResponseAdminGetStockProductDTO>>(products);
-    }
-    public async Task<List<ResponseAdminGetStockProductDTO>> GetAllLowStockProducts(int threshold = 5)
-    {
-        var products = await _productRepsository.GetAllLowStockProducts(threshold);
-        return _mapper.Map<List<ResponseAdminGetStockProductDTO>>(products);
-    }
+        var result = await _productVariantRepsository.GetAllVariantsForAdmin(request);
+        var products = result.Items;
+        var response = _mapper.Map<List<ResponseGetAllProductVariant>>(products);
+        for (int i = 0; i < products.Count; i++)
+        {
+            var validation = await _productValidation.ValidateProductChain(products[i].Product!);
 
-    public async Task<List<ResponseAdminGetAllProductDTO>> GetAllProductsWithPendingVariants()
-    {
-        var products = await _productRepsository.GetAllProductsWithPendingVariants();
-        return _mapper.Map<List<ResponseAdminGetAllProductDTO>>(products);
-    }
+            response[i].IsAvailableForSale = products[i].ProductApprovalStatusId == (int)ProductApprovalStatusEnum.Admin_Approved && products[i].ProductVariantStatusId == (int)ProductStatusEnum.Active
+            && validation.IsValid && products[i].Inventories.Any(inv => inv.AvailableQuantity > 0);
 
+            response[i].ValidationIssues = validation.Issues;
+        }
+        if (request.hasIssues.HasValue)
+        {
+            if (request.hasIssues.Value)
+            {
+                response = response.Where(p => p.ValidationIssues.Any()).ToList();
+            }
+            else
+            {
+                response = response.Where(p => !p.ValidationIssues.Any()).ToList();
+            }
+        }
+        if (request.isAvailableForSale.HasValue)
+        {
+            response = response.Where(p => p.IsAvailableForSale == request.isAvailableForSale.Value).ToList();
+        }
+        return new PagedResponse<ResponseGetAllProductVariant>
+        {
+            Items = _mapper.Map<List<ResponseGetAllProductVariant>>(products),
+            TotalCount = result.TotalCount,
+            PageNumber = request.PageNumber,
+            PageSize = request.PageSize
+        };
+    }
     public async Task<ResponseAdminGetAllProductDTO> GetProductWithFullDetails(int productId)
     {
         var product = await _productRepsository.GetProductWithFullDetails(productId);
