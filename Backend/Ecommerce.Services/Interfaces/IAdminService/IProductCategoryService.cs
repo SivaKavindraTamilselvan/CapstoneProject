d53f90706f4aedca67e81a16da66abef2694a6f7
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-12 14:32:20 +0530

    changed the return type and function names

--- Backend/Ecommerce.Services/Interfaces/IAdminService/IProductCategoryService.cs
+++ Backend/Ecommerce.Services/Interfaces/IAdminService/IProductCategoryService.cs
@@ -8,8 +8,8 @@ public interface IAdminProductCategoryService
     public Task<ResponseAdminGetAllSubCategory> ActivateProductSubCategory(int productSubCategoryId);
     public Task<ResponseAdminGetAllSubCategory> DeactivateProductSubCategory(int productSubCategoryId);
     public Task<ResponseAdminGetAllCategory> DeactivateProductCategory(int productCategoryId);
-    public Task<List<ResponseAdminGetAllSubCategory>> GetAllProductSubCategoryForAdminGetAllSubProductCategoryForAdmin(bool? status, int? categoryId, int pageNumber, int pageSize);
-    public Task<List<ResponseAdminGetAllCategory>> GetAllProductCategoryForAdmin(bool? status, int pageNumber, int pageSize);
+    public Task<PagedResponse<ResponseAdminGetAllSubCategory>> GetAllSubProductCategoryForAdmin(RequestProductSubCategoryFilter request);
+    public Task<PagedResponse<ResponseAdminGetAllCategory>> GetAllProductCategoryForAdmin(RequestProductCategoryFilter request);
     public Task<ResponseAddProductCategoryDTO> AddProductCategory(RequestAddProductCategoryDTO requestAddProductCategoryDTO, int adminuserid);
     public Task<ResponseAddProductSubCategoryDTO> AddProductSubCategory(RequestAddProductSubCategoryDTO requestAddProductSubCategoryDTO, int adminuserid);
 }
