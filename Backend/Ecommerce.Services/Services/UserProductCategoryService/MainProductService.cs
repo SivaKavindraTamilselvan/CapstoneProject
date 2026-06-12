f268d7b13b9b16bd4e9f4aca3a313f4bc2a25d1b
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-13 02:57:35 +0530

    added dependencies

--- Backend/Ecommerce.Services/Services/UserProductCategoryService/MainProductService.cs
+++ Backend/Ecommerce.Services/Services/UserProductCategoryService/MainProductService.cs
@@ -1,15 +1,18 @@
 using AutoMapper;
 using Ecommerce.Repositories.Interfaces;
 using Ecommerce.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 
 public partial class UserProductService : IUserProductService
 {
+    private readonly ILogger<UserProductService> _logger;
     private readonly IProductRepsository _productRepository;
     private readonly IMapper _mapper;
 
-    public UserProductService(IProductRepsository productRepository, IMapper mapper)
+    public UserProductService(ILogger<UserProductService> logger,IProductRepsository productRepository, IMapper mapper)
     {
         _productRepository = productRepository;
         _mapper = mapper;
+        _logger = logger;
     }
 }
