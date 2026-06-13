f7a88beeafb5411f4bb011dc0697eb1c1fe2d334
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-13 15:06:27 +0530

    params is changed

--- Backend/Ecommerce.Services/Interfaces/IReturnService/IUserReturnService.cs
+++ Backend/Ecommerce.Services/Interfaces/IReturnService/IUserReturnService.cs
@@ -4,5 +4,6 @@ namespace Ecommerce.Services.Interfaces;
 
 public interface IUserReturnService
 {
+    public Task<PagedResponse<ReturnSummaryDto>> GetAllReturnsForUser(RequestUserReturnFilter request, int UserId);
     public Task<ResponseAddReturnDTO> AddReturn(RequestAddReturnDTO requestAddReturnDTO);
 }
