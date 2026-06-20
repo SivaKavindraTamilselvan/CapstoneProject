ef66e451896dc89cf75214d785e319afa1d65ce0
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-21 03:28:06 +0530

    removed unwanted packages

--- MainFrontend/Ecommerce/src/app/register/register.ts
+++ MainFrontend/Ecommerce/src/app/register/register.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RegisterModel } from '../models/register-user.model';
 import { AuthService } from '../services/auth.Service';
 import { Router } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormField, email, form, pattern, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule, FormField, ReactiveFormsModule],
+  imports: [FormField],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
@@ -15,9 +14,14 @@ export class Register {
   errorMessage = signal<string | null>(null);
   registerModel = signal(new RegisterModel());
   progress = signal(false);
+  showPassword = signal(false);
   constructor(private router: Router, private authService: AuthService) {
 
   }
+  togglePasswordVisibility() {
+    this.showPassword.update(v => !v);
+  }
+
   registerForm = form(this.registerModel, (path) => {
     required(path.email, { message: "Enter The Email" })
     required(path.firstName, { message: "Enter The FirstName" })
@@ -33,6 +37,7 @@ export class Register {
       alert("Enter Proper Details");
       return;
     }
+    this.errorMessage.set(null);
     this.progress.set(true);
     this.authService.registerUserAPICall(this.registerModel()).subscribe({
       next: (response: any) => {
@@ -43,8 +48,7 @@ export class Register {
         console.error(error);
         this.progress.set(false);
         this.errorMessage.set(null);
-        if(error.status == 409)
-        {
+        if (error.status == 409) {
           this.errorMessage.set(error.error.message)
         }
         else if (error.status == 401) {
@@ -62,5 +66,9 @@ export class Register {
         }
       }
     })
+  }
+  resetFilter() {
+    this.registerModel.set(new RegisterModel());
+    this.errorMessage.set(null);
   }
 }
