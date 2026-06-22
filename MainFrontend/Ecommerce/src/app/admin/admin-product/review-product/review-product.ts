7aa29e3d59f8662e05d9cf818b50f38dd1eb900a
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-06-22 13:30:25 +0530

    added responsiveness

--- MainFrontend/Ecommerce/src/app/admin/admin-product/review-product/review-product.ts
+++ MainFrontend/Ecommerce/src/app/admin/admin-product/review-product/review-product.ts
@@ -10,7 +10,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-review-product',
-  imports: [ReactiveFormsModule],
+  imports: [FormField,ReactiveFormsModule],
   templateUrl: './review-product.html',
   styleUrl: './review-product.css',
 })
